849eb5cbf95971946f9c02294582dcd3add878f1
kbperez <kbpgperez@hotmail.com>
2017-12-21 11:16:58 -0800

    nav working

--- public/scripts/views/menuView.js
+++ public/scripts/views/menuView.js
@@ -1,5 +1,28 @@
 'use strict';
 
-$('.hamburger').on('click', function() {
-  $('nav ul').toggle();
-});
+
+let handleNav = () => {
+  $('.hamburger').on('click', function() {
+    $('nav ul').toggle();
+  });
+
+  $('#logout').on('click', function() {
+    localStorage.clear();
+    app.loginView.initLoginPage();
+  })
+
+  $('#home').on('click', function() {
+    app.movieView.initWatchlistPage();
+  })
+
+  $('#retest').on('click', function() {
+    let user = app.User.getOne(JSON.stringify(localStorage.username));
+    console.log('logged in user', user);
+    app.movieView.initTestPage(user);
+  })
+}
+
+
+$(document).ready(function(){
+  handleNav();
+})

2be4fd71d258dea42b65d85d1e6a2f0ce7bfc112
Roger Davenport <roger.neil.davenport@gmail.com>
2017-12-21 13:48:13 -0800

    fixed reference to api

--- public/scripts/models/user.js
+++ public/scripts/models/user.js
@@ -3,7 +3,7 @@
 var app =  app || {};
 
 // var __API_URL__ = 'http://localhost:3000';
-var __API_URL__ = 'https://chill-fellows.herokuapp.com/';
+var __API_URL__ = 'https://chill-fellows.herokuapp.com';
 
 (function(module) {
 
